a6a7d357d6d098c28cf6bf347d080f10a9afbc3c
Sergey <xxsmotur@gmail.com>
2019-07-10 07:56:36 +0500

    update

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,21 @@
+use std::io::Read;
+use std::io::BufRead;
+use std::env;
 
 struct cat {
 
 }
 
 impl cat {
-    fn version() -> &'static str {
+    fn version(self) -> &'static str {
         return "0.0.1"
+    }
+
+    fn read(self, name: &str) {
+
     }
 }
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    println!("{:?}", args);
 }
